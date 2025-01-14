371968f1754ebae90dec6e310f3ba9f0951f05f9
caumond <anthony@caumond.com>
2025-01-14 19:25:53 +0100

    repl

--- env/dev/src/clj/auto_build/repl/entry_point.clj
+++ env/dev/src/clj/auto_build/repl/entry_point.clj
@@ -1,15 +1,15 @@
 (ns auto-build.repl.entry-point
   "Entry point for repl"
-  (:require [cider.nrepl.middleware :as mw]
-            [auto-build.os.exit-codes :as exit-codes]
-            [nrepl.server :refer [default-handler start-server]]
+  (:require [auto-build.os.exit-codes :as exit-codes]
             [auto-build.os.colorized-text :refer [bg-red style-reset-all]]
-            [clojure.string :as str]
             [auto-build.repl.port-number :refer [port-number]]
+            [cider.nrepl.middleware :as mw]
+            [clojure.string :as str]
+            [nrepl.server :refer [default-handler start-server]]
             [refactor-nrepl.middleware]))
 
 (def custom-nrepl-handler
-  "We build our own custom nrepl handler, mimicking CIDER's."
+  "Custom nrepl handler."
   (apply default-handler
     (conj cider.nrepl.middleware/cider-middleware
           'refactor-nrepl.middleware/wrap-refactor)))
@@ -18,13 +18,10 @@
   "Entry point"
   [& _args]
   (let [port (port-number)]
-    (println "Start repl on port" port)
+    (println "Start repl on port:" port)
     (try
-      (start-server :port
-                    port
-                    :handler
-                    custom-nrepl-handler
-                    (spit ".nrepl-port" port))
+      (start-server :port port :handler custom-nrepl-handler)
+      (spit ".nrepl-port" port)
       (catch Exception e
         (when (str/includes? (ex-message e) "Address already in use")
           (println bg-red "Stopped as adress is already in use" style-reset-all)
