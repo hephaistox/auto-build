6314d7d97c5bda17ff1aa3d45ef06c44c40c924b
caumond <anthony@caumond.com>
2025-01-14 19:16:38 +0100

    repl

--- env/dev/src/clj/auto_build/repl/entry_point.clj
+++ env/dev/src/clj/auto_build/repl/entry_point.clj
@@ -1,7 +1,10 @@
 (ns auto-build.repl.entry-point
   "Entry point for repl"
   (:require [cider.nrepl.middleware :as mw]
+            [auto-build.os.exit-codes :as exit-codes]
             [nrepl.server :refer [default-handler start-server]]
+            [auto-build.os.colorized-text :refer [bg-red style-reset-all]]
+            [clojure.string :as str]
             [auto-build.repl.port-number :refer [port-number]]
             [refactor-nrepl.middleware]))
 
@@ -15,10 +18,14 @@
   "Entry point"
   [& _args]
   (let [port (port-number)]
-    (println "Automaton build development mode")
     (println "Start repl on port" port)
-    (start-server :port
-                  port
-                  :handler
-                  custom-nrepl-handler
-                  (spit ".nrepl-port" port))))
+    (try
+      (start-server :port
+                    port
+                    :handler
+                    custom-nrepl-handler
+                    (spit ".nrepl-port" port))
+      (catch Exception e
+        (when (str/includes? (ex-message e) "Address already in use")
+          (println bg-red "Stopped as adress is already in use" style-reset-all)
+          (System/exit exit-codes/misuse))))))
