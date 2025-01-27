7cdf69eac2e1af18c9e57d40fdc9259f96e15654
caumond <anthony@caumond.com>
2025-01-27 12:29:03 +0100

    repl is called with -X, no more parameter file needed

--- src/auto_build/tasks/repl.clj
+++ src/auto_build/tasks/repl.clj
@@ -1,6 +1,7 @@
 (ns auto-build.tasks.repl
-  (:require [auto-build.os.cli-opts :as build-cli-opts]
-            [babashka.process :as p]))
+  (:require
+   [auto-build.os.cli-opts :as build-cli-opts]
+   [babashka.process       :as p]))
 
 ;; ********************************************************************************
 ;; *** Task setup
@@ -18,11 +19,17 @@
 ;; ********************************************************************************
 
 (defn repl
-  [{:keys [title], :as _printers} app-dir current-task]
+  [{:keys [title]
+    :as _printers}
+   app-dir
+   current-task]
   (title "Start clj repl")
-  (when verbose (println "Execute [clojure -M:repl]"))
+  (when verbose (println "Execute [clojure -X:repl]"))
   (let [exit-code (build-cli-opts/enter cli-opts current-task)]
     (if exit-code
       exit-code
-      (-> (p/shell {:continue true, :dir app-dir} "clojure" "-M:repl")
+      (-> (p/shell {:continue true
+                    :dir app-dir}
+                   "clojure"
+                   "-X:repl")
           :exit))))
